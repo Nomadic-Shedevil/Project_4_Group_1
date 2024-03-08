019140771bb2362c67bde2480d328c1158ccccb0
Ryan <144743491+Ryo-333@users.noreply.github.com>
2024-03-07 19:19:02 -0600

    Update app.py

--- Static/app.py
+++ Static/app.py
@@ -1,27 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
-import numpy as np
 
 app = Flask(__name__)
 
-# Load the trained Random Forest model
-model_path = '../rf_model.pkl'  
+model_path = '../rf_model.pkl'
 with open(model_path, 'rb') as model_file:
     model = pickle.load(model_file)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    prediction = None
-    if request.method == 'POST':
-        # Extract and prepare form data for prediction
-        form_data = [int(request.form[attribute]) for attribute in request.form]
-        prediction = model.predict([form_data])[0]
-        # Translate prediction into human-readable format
-        prediction_text = 'Edible' if prediction == 0 else 'Poisonous'
-    else:
-        prediction_text = None
-
-    return render_template('index.html', prediction=prediction_text)
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.form
+    form_data = [int(data[attribute]) for attribute in data]
+    prediction = model.predict([form_data])[0]
+    result = 'Edible' if prediction == 0 else 'Poisonous'
+    return jsonify(result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
