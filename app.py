7bbe6b8f0c117288b831e5553c417674d63b652d
Ryan <144743491+Ryo-333@users.noreply.github.com>
2024-03-07 20:10:31 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import pickle
 
 app = Flask(__name__)
 
-@app.route('/')
+model_path = './rf_model.pkl'
+with open(model_path, 'rb') as model_file:
+    model = pickle.load(model_file)
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    data = request.form
+    form_data = [int(data[attribute]) for attribute in data]
+    prediction = model.predict([form_data])[0]
+    result = 'Edible' if prediction == 0 else 'Poisonous'
+    return jsonify(result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
